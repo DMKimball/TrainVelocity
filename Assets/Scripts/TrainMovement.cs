d7f2f2334fbe9b56cd567729203878f0715ed0b5
Carter <andrewscart@gmail.com>
2018-02-24 17:15:36 -0800

    i dont even know anymore
    what is going on

--- Assets/Scripts/TrainMovement.cs
+++ Assets/Scripts/TrainMovement.cs
@@ -11,25 +11,31 @@ public class TrainMovement : MonoBehaviour {
     [SerializeField] private Grabbable[] looseObjects;
 
     private float speed;
-
+    public float maxSpeed;
+    spedometer speedy;
+    heatManagement heat;
 	// Use this for initialization
 	void Start () {
         speed = DefaultSpeed;
+        speedy = GameObject.FindObjectOfType<spedometer>();
+        heat = GetComponent<heatManagement>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
         speed = Mathf.Max(speed - DecelerationPerSecond * Time.deltaTime, DefaultSpeed);
         transform.position += transform.forward * speed * Time.deltaTime;
-        
+        speedy.setSpeed(speed / maxSpeed);
         foreach(Grabbable obj in looseObjects)
         {
             obj.Translate(transform.forward * speed * Time.deltaTime);
         }
+
 	}
 
     public void AddFuel()
     {
         speed += AccelerationPerFuel;
+        heat.addFuel();
     }
 }
---
+++ Assets/Scripts/heatManagement.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor.SceneManagement;
+public class heatManagement : MonoBehaviour {
+    float heatLevel=0;
+    HeatMeter heatMeter;
+    public int heatReductionRate;
+    float heatReductionProgress=0;
+	// Use this for initialization
+	void Start () {
+        heatMeter = GameObject.FindObjectOfType<HeatMeter>();
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        if (heatReductionProgress > heatReductionRate)
+        {
+            heatReductionProgress = 0;
+            heatLevel=Mathf.Clamp(--heatLevel,0,100);
+            heatMeter.setHeat(heatLevel / 100);
+        }
+        else
+        {
+            heatReductionProgress++;
+        }
+        if (heatLevel>100)
+        {
+            Application.LoadLevel(Application.loadedLevel);
+        }
+	}
+    public void addFuel()
+    {
+        heatLevel += 15;
+        heatMeter.setHeat(heatLevel / 100);
+    }
+}
